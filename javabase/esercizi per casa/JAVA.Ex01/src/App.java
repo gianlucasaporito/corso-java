d4f5bdc836ea49090b9c95103a84bb38abf7e007
gianlucasaporito <g.saporito@neotech.srl>
2023-11-03 20:07:31 +0100

    javabase correzione esercizio01

---
+++ javabase/esercizi per casa/JAVA.Ex01/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {   ;
+     Automobile auto= new Automobile();
+      auto.marca="Fiat";
+      auto.modello="Topolino";
+      auto.colore="nero";
+      auto.cilindrata=369;
+        
+       System.out.println(auto);
+    }
+}
---
+++ javabase/esercizi per casa/JAVA.Ex01/src/Automobile.java
@@ -0,0 +1,13 @@
+public class Automobile {
+    public String marca;
+    public String modello;
+    public String colore;
+    public int cilindrata;
+    @Override
+    public String toString() {
+        return "Automobile [marca=" + marca + ", modello=" + modello + ", colore=" + colore + ", cilindrata="
+                + cilindrata + "]";
+    }
+
+     
+}
