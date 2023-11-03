46c03df4def20101f6d6f66e1e4b2f9d3f68a490
gianlucasaporito <g.saporito@neotech.srl>
2023-11-03 16:47:29 +0100

    javabase01

---
+++ javabase/esercizi per casa/esercizio01/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String marca="fiat ";
+        String modello="topolino " ;
+        String colore="nero ";
+        int cilindrata=569;
+    ;
+        
+        System.out.println(marca+modello+colore+cilindrata);
+    }
+}
