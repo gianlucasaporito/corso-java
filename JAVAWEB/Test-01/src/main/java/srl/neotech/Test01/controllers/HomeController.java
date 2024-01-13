2602dab5ee671781eee4c24a1679ad2f8b1f5f13
gianlucasaporito <g.saporito@neotech.srl>
2024-01-13 12:45:46 +0100

    test 01

--- JAVAWEB/Test-01/src/main/java/srl/neotech/Test01/controllers/HomeController.java
+++ JAVAWEB/Test-01/src/main/java/srl/neotech/Test01/controllers/HomeController.java
@@ -2,19 +2,14 @@ package srl.neotech.Test01.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
 
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String parametri (@RequestParam("numero")Integer num, @RequestParam("stringa")String str,Model model){
-        model.addAttribute("str", str);
-        model.addAttribute("num", num);
-        
-        return "test-01";
+    public String parametri (){
+        return "pagina";
     }
-
+    
 }
---
+++ JAVAWEB/autosalone/src/main/java/srl/neotech/autosalone/AutosaloneApplication.java
@@ -0,0 +1,13 @@
+package srl.neotech.autosalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosaloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosaloneApplication.class, args);
+	}
+
+}
